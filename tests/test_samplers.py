8c071bd7df2e80f69ef1dcda12f0e1eeb964d95c
yogesh1q2w <yogesh120699@gmail.com>
2025-10-20 08:31:09 +0200

    test after clean

--- tests/test_samplers.py
+++ tests/test_samplers.py
@@ -11,20 +11,21 @@ class PrioritizedSamplingTest(parameterized.TestCase):
 
     def setUp(self):
         super().setUp()
-        self.sampler = samplers.PrioritizedSamplingDistribution(seed=0, max_capacity=10)
+        self.sampler = samplers.Prioritized(seed=0, max_capacity=10)
 
     def test_sample(self):
         keys = [0, 1, 2, 3, 4]
         priorities = [1.0, 2.0, 3.0, 4.0, 0.0]
 
         for key, priority in zip(keys, priorities):
-            self.sampler.add(key, priority=priority)
+            self.sampler.add(key)
+            self.sampler.update(key, priority)
 
         # test if zero priority absent
         samples = self.sampler.sample(5)
         np.testing.assert_array_less(samples, 4)
 
-        self.sampler.update(keys=np.array([2, 3]), priorities=np.array([0.0, 0.0]))
+        self.sampler.update(keys=np.array([2, 3]), loss=np.array([0.0, 0.0]))
 
         # test if priority updated properly
         samples = self.sampler.sample(5)
@@ -33,7 +34,3 @@ class PrioritizedSamplingTest(parameterized.TestCase):
         self.sampler.remove(0)
         samples = self.sampler.sample(5)
         np.testing.assert_array_almost_equal(samples, 1)
-
-
-if __name__ == "__main__":
-    absltest.main()
